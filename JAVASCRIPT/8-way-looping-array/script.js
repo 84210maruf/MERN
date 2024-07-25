b32cc3f3e505635ed3a7873802ee181979782b59
maruf <maruf84210cse@gmail.com>
2024-07-25 22:44:24 +0600

    javacript array looping

--- JAVASCRIPT/8-way-looping-array/script.js
+++ JAVASCRIPT/8-way-looping-array/script.js
@@ -2,15 +2,15 @@ var index = 0
 var a = [10,20,30,40,50,60,70,80,90,100]
 
 console.log(a)
-console.log('++++-------------+++-------------++++')
 
 
+console.log('++++-------------+ for loop +-------------++++')
 for(index; index < a.length; index++){
     console.log(a[index])
 }
-console.log('++++-------------+++-------------++++')
 
 
+console.log('++++-------------+ while loop +-------------++++')
 index = 0
 
 while(index < a.length){
@@ -18,9 +18,9 @@ while(index < a.length){
     console.log(a[index])
     index++
 }
-console.log('++++-------------+++-------------++++')
 
 
+console.log('++++-------------+ forEach loop +-------------++++')
 index = 0
 var item = 0
 
@@ -30,17 +30,31 @@ function myfunc(item,index){
     
     console.log(index,item)
 }
-console.log('++++-------------+++-------------++++')
 
+console.log('++++-------------+forEach+-------------++++')
 a.forEach(element => {
     console.log(element)
 });
-console.log('++++-------------+++-------------++++')
 
 
+console.log('++++-------------+ map func +-------------++++')
 index = 0
 
-var square = x => Math.pow(n, 2)
-
+var square = x => Math.pow(x, 2)
 var squares = a.map(square)
+
 console.log(squares)
+squares.forEach(element => {console.log(element)})
+
+
+console.log('++++-------------+ every func +-------------++++')
+
+var under_five = x => x < 5
+
+if(a.every(under_five)){
+    console.log('some number in array is under five')
+}else{
+    console.log('some number in array is over five')
+}
+var every_return = a.every(under_five)
+console.log(typeof(every_return))
