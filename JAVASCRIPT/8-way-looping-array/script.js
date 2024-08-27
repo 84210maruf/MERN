c03013ed4a23b11884be3ccb90e72f2f0734df2e
maruf <maruf84210cse@gmail.com>
2024-08-27 14:25:52 +0600

    lopping

--- JAVASCRIPT/8-way-looping-array/script.js
+++ JAVASCRIPT/8-way-looping-array/script.js
@@ -1,11 +1,11 @@
 var index = 0
-var a = [10,20,30,40,50,60,70,80,90,100]
+var a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 console.log(a)
 
 
 console.log('++++-------------+ for loop +-------------++++')
-for(index; index < a.length; index++){
+for (index; index < a.length; index++) {
     console.log(a[index])
     // document.getElementById("demo_two").innerHTML = a
 }
@@ -14,8 +14,8 @@ for(index; index < a.length; index++){
 console.log('++++-------------+ while loop +-------------++++')
 index = 0
 
-while(index < a.length){
-    
+while (index < a.length) {
+
     console.log(a[index])
     index++
 }
@@ -27,9 +27,9 @@ var item = 0
 
 a.forEach(myfunc)
 
-function myfunc(item,index){
-    
-    console.log(index,item)
+function myfunc(item, index) {
+
+    console.log(index, item)
 }
 
 console.log('++++-------------+forEach+-------------++++')
@@ -45,32 +45,32 @@ var square = x => Math.pow(x, 2)
 var squares = a.map(square)
 
 console.log(squares)
-squares.forEach(element => {console.log(element)})
+squares.forEach(element => { console.log(element) })
 
 
 console.log('++++-------------+ every func +-------------++++')
 
 var under_five = x => x < 5
 
-if(a.every(under_five)){
+if (a.every(under_five)) {
     console.log('some number in array is under five')
-}else{
+} else {
     console.log('some number in array is over five')
 }
 var every_return = a.every(under_five)
-console.log(typeof(every_return))
+console.log(typeof (every_return))
 
 
 console.log('++++-------------+ filter +-------------++++')
 
-function newFunc(value,index,arr){
+function newFunc(value, index, arr) {
     return value > 50
 }
 
 var over50 = a.filter(newFunc)
 
 var index = 0
-while(index < over50.length){
+while (index < over50.length) {
     console.log(over50[index])
 
     index++
@@ -78,7 +78,7 @@ while(index < over50.length){
 
 console.log('++++-------------+ reduce func +-------------++++')
 
-function sumFunc(total,value,index,arr){
+function sumFunc(total, value, index, arr) {
 
     return total + value
 }
@@ -88,10 +88,10 @@ console.log(sum)
 
 
 console.log('++++-------------+ PRACTICE SESSION +-------------++++')
-var index = a.length - 1
-var n = 0
-var s = ['one','two','three','four','five','six','seven','eight','nine','ten']
-for(index; index >= 0; index--){
-    document.getElementById(s[n]).innerHTML = "<H1 style='color:blue'>" + a[index] + "<H1>"
-    n++
+var n = a.length
+var index = 0
+var s = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+for (index; index <= n; index++) {
+    document.getElementById(s[index]).innerHTML = "<H1 style='color:blue'>" + s[index] + "<H1>"
+    
 }
