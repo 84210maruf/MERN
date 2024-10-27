a546c9e716482818c6f5c68d84a6dd1d15360da7
maruf <maruf84210cse@gmail.com>
2024-10-27 20:22:10 +0600

    ready esop MERN backend API

---
+++ 7-EXPRESS/crud-api-app/models/product.model.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+
+
+const ProductSchema = new mongoose.Schema({
+    productId: {
+        type: Number,
+        required: [true, 'Please enter product name'],
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    session: {
+        type: String,
+        required: true,
+        enum: ["winter", "summer", "fall", "spring"]
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ["men", "women", "kids","accessories"]
+    },
+    subCategory: {
+        type: String,
+        trim: true
+    },
+    size: {
+        type: [String],
+        enum: ["XS", "S", "M", "L", "XL", "XXL"]
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: [String],
+        required: false,
+    },
+    discount: {
+        type: Number,
+        min: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    color: {
+        type: [String]
+    },
+    ratings: {
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
+
+// const mongoose = require("mongoose");
+
+// const ProductSchema = mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, "Please enter product name"],
+//     },
+
+//     quantity: {
+//       type: Number,
+//       required: true,
+//       default: 0,
+//     },
+
+//     price: {
+//       type: Number,
+//       required: true,
+//       default: 0,
+//     },
+
+//     image: {
+//       type: String,
+//       required: false,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+
+// const Product = mongoose.model("Product", ProductSchema);
+
+// module.exports = Product;
